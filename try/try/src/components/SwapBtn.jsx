9bfbf27eeee4e7330eebf781f06a66f2612c5deb
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-02-15 01:18:44 +0530

    done with context local storage

--- try/try/src/components/SwapBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SwapBtn = ({swap}) => {
-  return (
-    <button onClick={swap} className='bg-blue-700 text-white py-1 px-2 border rounded-md border-white text-lg mx-auto w-24 block cursor-pointer my-2 hover:brightness-90 hover:border-2 transition-all absolute'>Swap</button>
-  )
-}
-
-export default SwapBtn
